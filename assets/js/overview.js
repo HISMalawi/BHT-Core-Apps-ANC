911682b2b7261503a2c607f48b374d6deafe8502
Pachawo Bisani <phbisan@gmail.com>
2019-03-03 19:41:02 +0200

    Pull data from backend to overview page

--- assets/js/overview.js
+++ assets/js/overview.js
@@ -10,41 +10,97 @@ var programID = sessionStorage.programID;
 
 var sessionDate = sessionStorage.sessionDate;
 
-function getCurrentStatistics(){
-    
-  var url = apiProtocol + "://" + apiURL + ":" + apiPort 
-  url += "/api/v1/programs/"+programID+"/reports/visits"
-  url += "?name=visits_report&start_date="+sessionDate;
-    
-  var xhttp = new XMLHttpRequest();
-    
-  xhttp.onreadystatechange = function() {
-        
-    if (this.readyState == 4 && (this.status == 201 || this.status == 200)) {
-            
-      try{
-                
-        results = JSON.parse(this.responseText);
-        console.log(results);
-            
-      } catch(e){
+var user = {};
 
-        console.log(e);
+var today = {};
 
-      }
-        
-    }
-    
-  };
+var year = {};
+
+$(document).ready(function(){
   
-  xhttp.open("GET", url, true);
+  function getCurrentStatistics(){
     
-  xhttp.setRequestHeader('Authorization', sessionStorage.getItem("authorization"));
+    var url = apiProtocol + "://" + apiURL + ":" + apiPort 
+    url += "/api/v1/programs/"+programID+"/reports/visits"
+    url += "?name=visits_report&start_date="+sessionDate;
     
-  xhttp.setRequestHeader('Content-type', "application/json");
+    var xhttp = new XMLHttpRequest();
     
-  xhttp.send();
+    xhttp.onreadystatechange = function() {
+        
+      if (this.readyState == 4 && (this.status == 201 || this.status == 200)) {
+            
+        try{
+                
+          results = JSON.parse(this.responseText);
 
-}
+          year = results["year"];
+        
+          user = results["user"];
+        
+          today = results["today"];
 
-getCurrentStatistics();
+          var i = 1;
+
+          var k = 1;
+
+          var j = 1;
+
+          if (user !== undefined){
+
+            for(u in user){
+
+              document.getElementById('user_'+k).innerText = user[u];
+
+              k = k + 1;
+
+            }
+
+          }
+
+          if (today !== undefined){
+
+            for(t in today){
+
+              document.getElementById('today_'+j).innerText = today[t];
+  
+              j = j + 1;
+  
+            }
+
+          }
+        
+          if (year !== undefined){
+
+            for(y in year){
+
+              document.getElementById('year_'+i).innerText = year[y];
+
+              i = i + 1;
+
+            }
+
+          }
+        
+        } catch(e){
+
+          console.log(e);
+
+        }
+        
+      }
+    
+    };
+  
+    xhttp.open("GET", url, true);
+    
+    xhttp.setRequestHeader('Authorization', sessionStorage.getItem("authorization"));
+    
+    xhttp.setRequestHeader('Content-type', "application/json");
+
+    xhttp.send();
+
+  }
+
+  getCurrentStatistics();
+});
