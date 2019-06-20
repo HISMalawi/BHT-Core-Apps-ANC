7cacdacd0363fe45441bac3fb1e01eb5397f86c8
Pachawo Bisani <phbisan@gmail.com>
2019-06-20 16:51:08 +0200

    Changed current visit statistics overview

--- assets/js/overview.js
+++ assets/js/overview.js
@@ -40,6 +40,18 @@ $(document).ready(function(){
         
           today = results["today"];
 
+          user_visits = [user["1"],user["2"],user["3"],user["4"],user[">5"]]
+          today_visits = [today["1"],today["2"],today["3"],today["4"],today[">5"]]
+          year_visits = [year["1"],year["2"],year["3"],year["4"],year[">5"]]
+
+          visit_1 = [user["1"], today["1"], year["1"]]
+          visit_2 = [user["2"], today["2"], year["2"]]
+          visit_3 = [user["3"], today["3"], year["3"]]
+          visit_4 = [user["4"], today["4"], year["4"]]
+          visit_5 = [user[">5"], today[">5"], year[">5"]]
+
+          buildChart(visit_1,visit_2,visit_3,visit_4,visit_5,[ "Me", "Today", "This Year" ])
+/**
           var i = 1;
 
           var k = 1;
@@ -81,7 +93,7 @@ $(document).ready(function(){
             }
 
           }
-        
+*/        
         } catch(e){
 
           console.log(e);
@@ -103,4 +115,56 @@ $(document).ready(function(){
   }
 
   getCurrentStatistics();
+  //buildChart([ 0, 1, 2 ],[ 0, 0, 2],[ "Me", "Today", "This Year" ]);
 });
+
+function buildChart(one,two,three,four,five, days) {
+  // Build the chart
+          Highcharts.chart('chart-column', {
+              chart: {
+                  type: 'column'
+              },
+              title: {
+                  text: 'Current visit statistics'
+              },
+              credits: {
+                  enabled: false
+              },
+              exporting: {
+                  enabled: false
+              },
+              xAxis: {
+                  categories: days
+              },
+              yAxis: {
+                  allowDecimals: false,
+                  title: {
+                      text: 'Number of patients'
+                  }
+              },
+  
+              series: [{
+                  name: 'Visit 1',
+                  data: one
+              }, {
+                  name: 'Visit 2',
+                  data: two,
+                  lineWidth: 2
+              }, {
+                  name: 'Visit 3',
+                  data: three,
+                  lineWidth: 2
+              }, {
+                  name: 'Visit 4',
+                  data: four,
+                  lineWidth: 2
+              }, {
+                  name: 'Visit > 5',
+                  data: five,
+                  lineWidth: 2
+              }]
+          });
+  
+      }
+
+      
